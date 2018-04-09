b7af569e6aec09652ea53398e2bc5afc42523e63
maxwell2813 <maxwell281333@gmail.com>
2018-04-10 03:44:16 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const discord = require("discord.js");
-const talkedRecently = new Set();
 const client = new discord.Client();
 
 function commandIs(str, msg){
@@ -15,25 +14,37 @@ client.on('message', message => {
 	
     if(message.author.bot) return;
 	
-    if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
-
-          (message.content.includes("gay", message)){
-        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP'); 
-	  }
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(msg.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(msg.author.id);
-        }, 60000);
-    }	
-
+    if(message.content.includes("gay", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
     if(message.content.includes("Gay", message)){
         message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
     }
 
+    if(message.content.includes("GAy", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
+    if(message.content.includes("GAY", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
+    if(message.content.includes("GaY", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
+    if(message.content.includes("gaY", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
+    if(message.content.includes("gAy", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
+    if(message.content.includes("gaY", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
+	
 })
 
 // Allows the bot to login
