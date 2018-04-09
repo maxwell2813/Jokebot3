c9eb9d120aad4190f79049b8941af67955f87755
maxwell2813 <maxwell281333@gmail.com>
2018-04-10 03:14:11 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const discord = require("discord.js");
-
-// Makes the discord bot run in its own client
+const talkedRecently = new Set();
 const client = new discord.Client();
 
 function commandIs(str, msg){
@@ -14,15 +13,28 @@ client.on('ready', () => {
 client.on('message', message => {
     var args = message.content.split(/[ ]+/);
 	
-	if(message.author.bot) return;
+    if(message.author.bot) return;
 	
+    if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+
     if(message.content.includes("gay", message)){
         message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
     }
-    if(message.content.includes("Gay", message)){
-        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!***                                                                                                                                                                               https://imgur.com/gallery/f4jxP');
+        talkedRecently.add(msg.author.id);
+	    
+        setTimeout(() => {
+		
+		
+          talkedRecently.delete(msg.author.id);
+		
+        }, 60000);
     }
 
+    if(message.content.includes("Gay", message)){
+        message.channel.sendMessage('***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!*** \n \n https://imgur.com/gallery/f4jxP');
+    }
 })
 
 // Allows the bot to login
