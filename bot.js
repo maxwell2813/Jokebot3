e32718995071948dbc34d227d1c89472891a024b
maxwell2813 <maxwell281333@gmail.com>
2018-09-04 19:25:35 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ function commandIs(str, msg){
 }
 
 function agay () {
-	var rand = ['***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!***','***WHAT DID YOU JUST SAY?!***','***~~I NEED MORE IDEAS FOR REPLYS DAMN IT~~***','***TYPE OVERUSED GAY JOKE HERE***','*quit spamming this fucking command*','ygaygsyagygygaygaygayagyagyagaygaygayagyagyagyagyagyagyaga','no u']
+	var rand = ['***DID SOMEONE JUST SAY GAY? LMAO YOU NIGGAS GAY!***','***WHAT DID YOU JUST SAY?!***','***~~I NEED MORE IDEAS FOR REPLYS DAMN IT~~***','***TYPE OVERUSED GAY JOKE HERE***','*quit spamming this fucking command*','ygaygsyagygygaygaygayagyagyagaygaygayagyagyagyagyagyagyaga','no u','Warning: User too gay for command to execute']
 	
 	return rand[Math.floor(Math.random()*rand.length)];
 }
